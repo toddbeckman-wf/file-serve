d27fdee445a24c08173205f9ed6f615b7a9d9b32
toddbeckman-wf <todd.beckman@workiva.com>
2016-01-19 12:37:36 -0700

    put class files in gitignore

--- server/config-dev.py
+++ server/config-dev.py
@@ -1,6 +1,6 @@
 # make a copy of this into config.py
 # ideally with different credentials
 port = 5080
-data = "../"
+data = "../files/"
 user = 'user'
 password = 'pass'
--- server/server.py
+++ server/server.py
@@ -1,2 +1,3 @@
 from config import *
 
+print (user, password, port)
