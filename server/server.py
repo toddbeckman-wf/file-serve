0465ac08f6ceeafaf58adea5fd4d6e1df2ed72ee
toddbeckman-wf <todd.beckman@workiva.com>
2016-01-19 13:02:41 -0700

    basic read and write files

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import os.path
 
 
 def getFile(filename):
-    name = data_loc + filename;
+    name = data_loc + filename
     if os.path.isfile(name):
         with open(name, 'r') as myfile:
             data = myfile.read()
@@ -11,4 +11,11 @@ def getFile(filename):
     else:
         print('Could not find',filename)
 
-print(getFile(raw_input('File name: ')))
+def putFile(filename, contents):
+    name = data_loc + filename
+    with open(name, 'w') as myfile:
+        myfile.write(contents)
+
+somedata = getFile(raw_input('File name: '))
+print(somedata)
+putFile('moredata.txt',somedata)
