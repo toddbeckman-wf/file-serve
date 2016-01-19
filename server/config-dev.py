715b5803dcbdd8d9785a264abf9e976e3900a5db
toddbeckman-wf <todd.beckman@workiva.com>
2016-01-19 12:53:34 -0700

    implemented preliminary getFile

--- server/config-dev.py
+++ server/config-dev.py
@@ -1,6 +1,6 @@
 # make a copy of this into config.py
 # ideally with different credentials
 port = 5080
-data = "../files/"
+data_loc = "../files/"
 user = 'user'
 password = 'pass'
--- server/server.py
+++ server/server.py
@@ -1,3 +1,14 @@
 from config import *
+import os.path
 
-print (user, password, port)
+
+def getFile(filename):
+    name = data_loc + filename;
+    if os.path.isfile(name):
+        with open(name, 'r') as myfile:
+            data = myfile.read()
+        return data
+    else:
+        print('Could not find',filename)
+
+print(getFile(raw_input('File name: ')))
