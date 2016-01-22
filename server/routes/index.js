e055d17976eff278b582ca4b827bbfae48d111d7
toddbeckman-wf <todd.beckman@workiva.com>
2016-01-22 16:26:41 -0700

    further progress on the general workflow

--- server/routes/index.js
+++ server/routes/index.js
@@ -24,39 +24,82 @@ var upload = multer({
 var docLoc = __dirname + "../../" + config.doc_loc;
 var pdfLoc = __dirname = "../../" + config.pdf_loc;
 
+// entry point: regular get request to the index
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Welcome',
+    salt: 1//eventually this can be used to ensure security
+  })
+}
+
+// TODO: this is very weak authentication
+// hash the transaction
+// add salt on both side of transaction?
 function authenticate(query){
-  //  TODO: this is very weak authentication
-  //  hash the transaction
-  //  add salt on both side of transaction?
-  return query.u && query.p &&
-    query.u == config.user &&
-    query.p == config.password;
-}
-
-function createJobs(res, file) {
-  var getKey = keys.addkey(file, 'get');
-  var putKey = keys.addkey(file, 'put');
-  var salt = Math.random();
-  res.json(JSON.stringify({
-    "get": getKey,
-    "set": setKey,
-    "salt": salt
-  }));
+  return query.user && query.pass &&
+    query.user == config.user &&
+    query.pass == config.password;
 }
 
 
-function getFile(res, query) {
+
+// Client's first action after entry is to request keys
+router.get('keys.json', function (req, res, next) {
+  var query = url.parse(req,url, true).query;
+  if (!authenticate(query)) {
+    res.json({
+      error: "error",
+      message: "Authentication failed. No keys were made."
+    })
+  }
+  else {
+    if (query.file) {
+      //  Associate keys with the filename
+      var getKey = keys.addkey(file, 'get');
+      var putKey = keys.addkey(file, 'put');
+      var salt = Math.random();
+
+      //  give those keys to the user
+      res.json(JSON.stringify({
+        "get": getKey,
+        "set": setKey,
+        "salt": salt
+      }));
+    }
+    else {
+      res.json({
+        error: "error",
+        message: "No file name requested. No keys were made."
+      })
+    }
+  })
+});
+
+
+// switch to post when security is added
+router.get('getfile', function(req, res, next) {
+  var query = url.parse(req.url, true).query;
+  
+  //  authenticate first
+  if (!authenticate(query)) {
+    res.render('error', {
+      title: 'Oops!',
+      message: 'Those credentials are not correct!'
+    })
+  }
+
+  //  Verify the job
   var job = keys.read(query.key);
   if (job.job != query.job) {
-    res.render('index', {
+    res.render('error', {
       title: 'Oops',
       message: 'Looks like your key is not meant for this job.'
     });
   }
-  else {
-    var filename = job.name;
 
+  else {
     // get the path
+    var filename = job.name;
     var path = docLoc + filename;
 
     // prepare the response header; need the size
@@ -70,10 +113,12 @@ function getFile(res, query) {
       // stream the file as it is loaded
       var readStream = fs.createReadStream(filePath);
       readStream.pipe(res);
+      keys.remove(query.key);
     }
+
     // the requested file was not found
     catch (e) {
-      res.render('index', { 
+      res.render('error', { 
         title: 'Oops!',
         message: 'The requested file ' + filename + ' was not found.'
       });
@@ -81,50 +126,11 @@ function getFile(res, query) {
   }
 }
 
-function putFile(req, res, query) {
-}
 
+// the handler for file uploads
 router.post('/upload', upload, function(req, res, next) {
-    var query = url.parse(req.url, true).query;
+    // todo
   }
-)
-
-// when a post request has been made
-router.post('/', function(req, res, next)) {
-  var query = url.parse(req.url, true).query;
-  if (!authenticate(query)) {
-    res.render('index', {
-      title: 'Oops!',
-      message: 'Those credentials are not correct!'
-    })
-  }
-
-  if (query.job) {
-    switch (query.job) {
-    case 'get':
-      getFile(res, query);
-      break;
-    case 'put':
-      putFile(req, res, query);
-      break;
-    default:
-      res.render('index', {
-        title: 'Oops!',
-        message: 'That job is not supported!'
-      })
-    }
-  }
-  else {
-    if (query.file) {
-      createJobs(res, query.file);
-    }
-    else {
-      res.render('index', {
-        title: 'Oops!',
-        message: 'You specified neither a job nor a file!'
-      })
-    }
-  }
-}
+);
 
 module.exports = router;
